0c71edf70823b1c7167bc5038de80b75a1f588ac
persexgithub <79389819+persexgithub@users.noreply.github.com>
2025-04-11 03:27:35 -0400

    finished all pages

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -3,15 +3,25 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import resumePDF from "../assets/Corbishley_Resume.pdf";
 
 import {Link} from 'react-router-dom';
+import style from '../styles/resumepage.module.css';
 
 
 function ResumePage() {
     return (
-        <>
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-            <Viewer fileUrl={resumePDF} />
-        </Worker>
-        </>
+        <div className={style["root"]}>
+            <div className={style["header"]}>
+                        <p>FXC</p> 
+                        <Link to="/About">
+                          <p className={style["resLink"]}>About</p>
+                        </Link>
+                        <a href="https://github.com/franxcor">GitHub</a>
+                      </div>
+            <div className={style["resumeDiv"]}>
+                <Worker className={style["resumeWorker"]} workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+                    <Viewer className={style["resumeViewer"]} fileUrl={resumePDF} />
+                </Worker>
+            </div>
+        </ div>
     )
 }
 
