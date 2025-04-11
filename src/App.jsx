13617e82bb77faabae35d3ee0faf6caa294d5ec2
persexgithub <79389819+persexgithub@users.noreply.github.com>
2025-04-11 00:02:28 -0400

    welcome done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
-    <div className="root">
+    <div>
+      <HomePage>
+      
+      </HomePage>
     </div>
+    
   )
 }
 
