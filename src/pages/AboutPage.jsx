6af10e2b0d310ff07ba2e38862c431413047b516
persexgithub <79389819+persexgithub@users.noreply.github.com>
2025-04-11 22:53:15 -0400

    finishing pages

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,17 +2,55 @@ import { useState } from "react"
 import {Link} from "react-router";
 
 import style from "../styles/aboutpage.module.css"
-import background from "../assets/aboutBackground.png"
-import frame from "../assets/photo.png"
-import education from "../assets/education.png"
-import skills from "../assets/skills.png"
-import experience from "../assets/experience.png"
+import frame from "../assets/framedPhoto.png"
+import bio from "../assets/bio.png"
+import education from "../assets/eduGroup.png"
+import skills from "../assets/skillsGroup.png"
+import experience from "../assets/ExpGroup.png"
+import fxc from "../assets/FXC.png"
+import home from "../assets/Home.png"
+import linked from "../assets/LinkedIn.png"
+import resume from "../assets/Resume.png"
+import github from "../assets/Github.png"
 
 function AboutPage() {
 
     return (
       <>
         <div className={style["root"]}>
+          <div className={style["header"]}>
+            <ul className={style["headList"]}>
+              <li className={style["listItem"]}><img src={fxc} className={style["fxc"]} style={{ marginRight: '80%' }} /> </li>
+              <li className={style["listItem"]}><img src={home} className={style["home"]} /></li>
+              
+              <li className={style["listItem"]}>
+                <Link to="/Resume">
+                <img src={resume} className={style["resume"]} /> 
+                </Link>
+              </li>
+              <li className={style["listItem"]}> 
+                <a href="https://www.linkedin.com/in/francesca-corbishley/">
+                <img src={linked} className={style["linked"]} /> </a>
+              </li>
+              <li className={style["listItem"]}>
+                <a href="https://github.com/franxcor">
+                <img src={github} className={style["github"]} />
+                </a>
+              </li>
+            </ul>
+            
+          </div>
+          
+          <div className={style["mainContent"]}>
+          <img src={frame} className={style["photo"]} />
+          <img src={bio} className={style["bio"]} />
+          </div>
+          
+          <img src={education} className={style["education"]}/>
+          <img src={skills} className={style["skills"]}/>
+          <img src={experience} className={style["experience"]}/>
+
+          {/*
           <div className={style["header"]}>
             <p>FXC</p> 
             <Link to="/Resume">
@@ -43,8 +81,9 @@ function AboutPage() {
             </div>
           </div>
           
-          
+          */}
         </div>
+        
       </>
     )
   }
