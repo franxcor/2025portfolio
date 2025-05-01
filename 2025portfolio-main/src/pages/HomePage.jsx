619738623c713c323488e698d4eb204f076022fa
franxcor <129801200+franxcor@users.noreply.github.com>
2025-05-01 15:04:07 -0400

    trying to deploy

---
+++ 2025portfolio-main/src/pages/HomePage.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import {Link} from "react-router";
+
+import style from "../styles/homepage.module.css"
+import welcome from "../assets/welcome.png"
+import name from "../assets/nameTag.png"
+import enter from "../assets/enterTicket.png"
+import enterShadow from "../assets/enterTicketShadow.png"
+import button from "../assets/button.png"
+import loveLetter from "../assets/loveLetter.png"
+
+function HomePage() {
+    const [isHovering, setIsHovering] = useState(false);
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    }
+
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    }
+
+    return (
+      <div className={style["root"]}>
+        <img src={welcome} className={style["welcome"]} />
+        <img src={loveLetter} className={style["loveLetter"]} />
+        <img src={name} className={style["name"]} />
+        {isHovering && (    
+        <img src={enterShadow} 
+            className={style["enterShadow"]} 
+        />
+        )}
+        <Link to="/About">
+            <img src={enter} className={style["enter"]} 
+            onMouseEnter={handleMouseOver}
+            onMouseLeave={handleMouseOut} />
+        </Link>
+        
+        
+        <img src={button} className={style["button"]} />
+      </div>
+    )
+  }
+  
+  export default HomePage
